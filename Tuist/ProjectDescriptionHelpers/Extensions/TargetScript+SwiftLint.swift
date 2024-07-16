e3a557525be70a863437e514425428bc0cb78057
Prokoptsov <prokoptsov@arcsinus.ru>
2024-07-16 14:10:08 +0300

    Удалил закомментированные описания скрипта SwiftLint

--- Tuist/ProjectDescriptionHelpers/Extensions/TargetScript+SwiftLint.swift
+++ Tuist/ProjectDescriptionHelpers/Extensions/TargetScript+SwiftLint.swift
@@ -14,15 +14,4 @@ public extension TargetScript {
 		name: "SwiftLint",
 		basedOnDependencyAnalysis: false
 	)
-	//	static let swiftLint: TargetScript = .pre(
-	//		script:
-	// """
-	// if which swiftlint >/dev/null; then
-	// swiftlint lint --config .swiftlint.yml
-	// else
-	// echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-	// fi
-	// """,
-	//		name: "SwiftLint"
-	//	)
 }
